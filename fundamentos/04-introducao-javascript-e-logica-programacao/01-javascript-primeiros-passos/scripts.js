1ced4556cbce8cbfe98d9de2bf8594b528e0aed1
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-02 00:25:57 -0300

    exercicio 4.1 fixacao - if else requisito 1

--- fundamentos/04-introducao-javascript-e-logica-programacao/01-javascript-primeiros-passos/scripts.js
+++ fundamentos/04-introducao-javascript-e-logica-programacao/01-javascript-primeiros-passos/scripts.js
@@ -1,3 +1,4 @@
+// Exercícios Fixação - Variáveis
 let myName = "Ibrahim";
 const birthCity = "São Paulo";
 let birthYear = 1993;
@@ -7,9 +8,13 @@ birthYear = 2030;
 
 console.log(myName, birthCity, birthYear);
 
+// Exercícios Fixação - Tipos
 const base = 5;
 const height = 8;
 const area = base * height;
 const perimeter = 2 * (base + height);
 
 console.log(area, perimeter);
+
+// Exercícios Fixação - If Else
+const grade = 55;
