7c1c2f05490269a9a1db38ff37a4dab7bd935d1d
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-02 00:56:11 -0300

    exercicio 4.1 fixacao - operadores requisito 3

--- fundamentos/04-introducao-javascript-e-logica-programacao/01-javascript-primeiros-passos/scripts.js
+++ fundamentos/04-introducao-javascript-e-logica-programacao/01-javascript-primeiros-passos/scripts.js
@@ -56,8 +56,14 @@ else if (currentHour > 4 && currentHour < 11){
 console.log(message);
 
 // Exercícios Fixação - Operadores ||
-let weekDay = "quarta-feira";
+let weekDay = "domingo";
 
 if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira"){
     console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}
+else if (weekDay === "sábado" || weekDay === "domingo") {
+    console.log("FINALMENTE, descanso merecido UwU");
+}
+else {
+    console.log("Ué, não conheço esse dia da semana :(");
 }
