e08aac507fc08e9f75c5d998abad459cff4c468c
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-02 15:06:08 -0300

    exercicio 4.1 pratico - requisito 1

---
+++ fundamentos/04-introducao-javascript-e-logica-programacao/01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,10 @@
+const a = 2;
+const b = 5;
+
+let sum = a + b;
+let subtract = a - b;
+let multiply = a * b;
+let divide = a / b;
+let modulus = a % b;
+
+console.log (sum, subtract, multiply, divide, modulus);
