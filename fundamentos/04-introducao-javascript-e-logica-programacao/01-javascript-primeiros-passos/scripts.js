3eee8124260cd12606d72409d8cd2c45cb7ebbc4
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-02 15:09:51 -0300

    exercicio 4.1 pratico - requisito 2

--- fundamentos/04-introducao-javascript-e-logica-programacao/01-javascript-primeiros-passos/scripts.js
+++ fundamentos/04-introducao-javascript-e-logica-programacao/01-javascript-primeiros-passos/scripts.js
@@ -8,3 +8,16 @@ let divide = a / b;
 let modulus = a % b;
 
 console.log (sum, subtract, multiply, divide, modulus);
+
+const x = 5;
+const y = 7;
+
+if(x > y){
+  console.log ("x é maior que y");
+}
+else if(y > x){
+  console.log ("y é maior que x");
+}
+else{
+  console.log("x e y são iguais");
+}
