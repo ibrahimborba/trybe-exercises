781ed6ad64711c8016705cbc6691b400b1f36f8e
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-03 13:56:41 -0300

    exercicio 4.2 - bonus 1

--- fundamentos/04-introducao-javascript-e-logica-programacao/02-javascript-array-loop-for/scripts.js
+++ fundamentos/04-introducao-javascript-e-logica-programacao/02-javascript-array-loop-for/scripts.js
@@ -78,3 +78,15 @@ for(let index = 0; index < array.length; index += 1){
   arrayDivide.push(array[index]/2); 
 }
 console.log(arrayDivide);
+
+/*Ordena em ordem crescente*/
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+console.log(numbers);
