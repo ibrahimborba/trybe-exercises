fb0ff00f7ce5d696d723aecb74ef98b8f8952050
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-03 14:59:07 -0300

    exercicio 4.2 - bonus 3

--- fundamentos/04-introducao-javascript-e-logica-programacao/02-javascript-array-loop-for/scripts.js
+++ fundamentos/04-introducao-javascript-e-logica-programacao/02-javascript-array-loop-for/scripts.js
@@ -102,3 +102,13 @@ for (let index = 1; index < numbers.length; index += 1) {
   }
 }
 console.log(numbers);
+
+/*Multiplica pelo elemento seguinte*/
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let multiplyByNext = [];
+for(let index = 0; index < numbers.length-1; index += 1){
+    multiplyByNext.push(numbers[index]*(numbers[index+1]));
+}
+multiplyByNext.push(numbers[numbers.length - 1]*2);
+console.log(multiplyByNext);
+
