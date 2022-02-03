ef05468aff33e650d12759140d1d6286c9267da2
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-03 15:53:02 -0300

    adiciona exercicio 2.1

---
+++ fundamentos/02-git-github-internet/01-git-github-oque-é-e-para-que-serve/exercicio.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Parte 1
+cat > trybe-skills.txt
+O que eu vou aprender na Trybe:
+
+- Unix
+- Bash
+- Git
+^D
+
+#Parte 2
+ls -l
+
+#Parte 3
+cd ..
+git init
+touch README.md
+git add .
+git commit -m "Initial commit"
