c8ef4272806257f284654f546c5c7233b47be136
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-04 00:57:45 -0300

    exercicio 4.3 requisito 2

--- fundamentos/04-introducao-javascript-e-logica-programacao/03-javascript-logica-e-algoritmos/scripts.js
+++ fundamentos/04-introducao-javascript-e-logica-programacao/03-javascript-logica-e-algoritmos/scripts.js
@@ -4,3 +4,12 @@ for (let counter = 1; counter <= 10; counter += 1){
   factorial *= counter;
 }
 console.log(factorial);
+
+// Inverte uma palavra
+let word = 'banana';
+let wordInverse = '';
+for (let i = word.length-1; i >= 0; i -= 1) {
+  wordInverse += word[i];
+}
+console.log('O inverso da palavra ' + word + " é " + wordInverse);
+
