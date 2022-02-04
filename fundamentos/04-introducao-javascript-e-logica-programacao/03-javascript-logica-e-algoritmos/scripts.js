f8a31e6c6e7cda085617477181968d192fcb1d40
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-04 01:08:51 -0300

    exercicio 4.3 requisito 3

--- fundamentos/04-introducao-javascript-e-logica-programacao/03-javascript-logica-e-algoritmos/scripts.js
+++ fundamentos/04-introducao-javascript-e-logica-programacao/03-javascript-logica-e-algoritmos/scripts.js
@@ -13,3 +13,25 @@ for (let i = word.length-1; i >= 0; i -= 1) {
 }
 console.log('O inverso da palavra ' + word + " é " + wordInverse);
 
+// Retorna maior e menor palavras dentro do array
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let arrayCount = [];
+for (let i = 0; i < array.length; i += 1){
+  let charCounter = 0;
+  for (let j = 0; j < array[i].length; j += 1) {
+    charCounter += 1;
+  }
+  arrayCount.push(charCounter);
+}
+
+let largest = 0;
+let smallest = 100;
+for (let i = 0; i < arrayCount.length; i += 1){
+  if(arrayCount[i] > largest){
+    largest = arrayCount[i];
+  }else if(arrayCount[i] < smallest){
+    smallest = arrayCount[i];
+  }
+}
+
+console.log('A maior palavra é ' + array[arrayCount.indexOf(largest)] + ' e a menor palavra é ' + array[arrayCount.indexOf(smallest)]);
