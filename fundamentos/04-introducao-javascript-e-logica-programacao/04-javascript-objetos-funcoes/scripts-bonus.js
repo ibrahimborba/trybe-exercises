af6393c6f6e1823abf6ae69e6102462604161190
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-06 21:04:16 -0300

    exercicios 4.4 - bonus requisito 4

--- fundamentos/04-introducao-javascript-e-logica-programacao/04-javascript-objetos-funcoes/scripts-bonus.js
+++ fundamentos/04-introducao-javascript-e-logica-programacao/04-javascript-objetos-funcoes/scripts-bonus.js
@@ -13,3 +13,37 @@ function arrayOfNumbers(vector) {
 
 let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
 console.log(arrayOfNumbers(vector));
+
+// Acesse as chaves nome , sobrenome , andar e apartamento do último morador do blocoDois
+let moradores = {
+  blocoUm: [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+  ],
+  blocoDois: [
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ],
+};
+
+console.log('O morador do bloco 2 de nome ' + moradores['blocoDois'][1]['nome'] + ' ' +  moradores['blocoDois'][1]['sobrenome'] + ' mora no ' + moradores['blocoDois'][1]['andar'] + '° andar, apartamento ' +  moradores['blocoDois'][1]['apartamento']);
