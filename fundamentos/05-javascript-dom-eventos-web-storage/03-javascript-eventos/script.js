85a20353552fde1e45378984abde347efc462862
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-11 02:48:55 -0300

    feat(requisito): exercicios praticos 5.3 requisito 1

--- fundamentos/05-javascript-dom-eventos-web-storage/03-javascript-eventos/script.js
+++ fundamentos/05-javascript-dom-eventos-web-storage/03-javascript-eventos/script.js
@@ -14,3 +14,21 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+// 01 - Cria lista com dias de dezembro
+let daysList = document.getElementById('days');
+
+for (let i in dezDaysList) {
+  let dayListItem = document.createElement('li');
+  dayListItem.innerHTML = dezDaysList[i];
+  dayListItem.className = 'day';
+  if (dayListItem.innerText === '24' || dayListItem.innerText === '31') {
+    dayListItem.className = 'day holiday';
+  } else if (dayListItem.innerText === '4' || dayListItem.innerText === '11' || dayListItem.innerText === '18') {
+    dayListItem.className = 'day friday';
+  } else if (dayListItem.innerText === '25') {
+    dayListItem.className = 'day friday holiday';
+  }
+  daysList.appendChild(dayListItem);
+}
