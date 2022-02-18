11322d671ea2394f0ddd37d03dd3ec0cbee63a8e
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-18 15:07:37 -0300

    feat(requisito): exercicios praticos 6.1 requisito bonus 1

--- fundamentos/06-html-css-forms-flexbox-responsivo/01-html-css-forms/script.js
+++ fundamentos/06-html-css-forms-flexbox-responsivo/01-html-css-forms/script.js
@@ -1,7 +1,13 @@
 const buttonSend = document.getElementById('send-form');
+const photoAgreement = document.getElementById('info-agreement');
 
 function getForm(event) {
-  event.preventDefault();
+  if (photoAgreement.checked !== true) {
+    event.preventDefault();
+    alert('Precisamos que autorize o uso de fotos pra prosseguir com sua candidatura');
+  } else {
+    alert('Seu formulário foi enviado!');
+  }
 }
 
 buttonSend.addEventListener('click', getForm);
