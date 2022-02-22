f879826c3d7f94c36009371fba4d464f9ce64f15
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-02-22 15:20:09 -0300

    feat(requisito): exercicios praticos 6.2 requisito 3

--- fundamentos/06-html-css-forms-flexbox-responsivo/02-bibliotecas-js-frameworks-css/script.js
+++ fundamentos/06-html-css-forms-flexbox-responsivo/02-bibliotecas-js-frameworks-css/script.js
@@ -1,3 +1,4 @@
+// Biblioteca Data
 let picker = new Pikaday({
   field: document.getElementById('travel-date'),
   format: 'DD/MM/YYYY',
@@ -7,3 +8,69 @@ let picker = new Pikaday({
     console.log(this.getMoment().format('Do MMMM YYYY'));
   }
 });
+
+// Biblioteca validação das informações
+const validation = new JustValidate('#form', {
+  errorFieldCssClass: 'is-invalid',
+  errorFieldStyle: {
+    border: '1px solid red',
+  },
+  errorLabelCssClass: 'is-label-invalid',
+  errorLabelStyle: {
+    color: 'red',
+    textDecoration: 'underlined',
+  }
+});
+
+validation
+  .addField('#name-surname', [
+    {
+      rule: 'minLength',
+      value: 10,
+    },
+    {
+      rule: 'maxLength',
+      value: 40,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Field is required',
+    },
+  ])
+  .addField('#email', [
+    {
+      rule: 'minLength',
+      value: 10,
+    },
+    {
+      rule: 'maxLength',
+      value: 50,
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Field is required',
+    },
+  ])
+  .addField('#why-text', [
+    {
+      rule: 'minLength',
+      value: 5,
+    },
+    {
+      rule: 'maxLength',
+      value: 500,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Field is required',
+    },
+  ])
+  .addField('#info-agreement', [
+    {
+      rule: 'required',
+    },
+  ]);
