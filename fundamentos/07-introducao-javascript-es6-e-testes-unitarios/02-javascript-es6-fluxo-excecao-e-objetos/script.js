ade02a62c6ea427b31963fa82fedacd63f20c1a2
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-03-03 15:48:29 -0300

    feat(requisito): exercicios praticos 7.2 parte 1 requisito 1

--- fundamentos/07-introducao-javascript-es6-e-testes-unitarios/02-javascript-es6-fluxo-excecao-e-objetos/script.js
+++ fundamentos/07-introducao-javascript-es6-e-testes-unitarios/02-javascript-es6-fluxo-excecao-e-objetos/script.js
@@ -1,12 +1,30 @@
+// Requisito 1 - verifica se os valores dos inputs são válidos
+
+const verifyInputs = (input1, input2) => {
+  if (input1 === '' || input2 === '') {
+    throw new Error('Preencha os dois campos com números');
+  } else if (isNaN(parseInt(input1)) === true || isNaN(parseInt(input2)) === true) {
+    throw new Error('Insira apenas números');
+  }
+}
+
 function sum() {
   const value1 = document.getElementById('value1').value;
   const value2 = document.getElementById('value2').value;
-  const result = parseInt(value1) + parseInt(value2);
-  document.getElementById('result').innerHTML = `Resultado: ${result}`;
-  document.getElementById('value1').value = '';
-  document.getElementById('value2').value = '';
+  try {
+    verifyInputs(value1, value2);
+    const result = parseInt(value1) + parseInt(value2);
+    document.getElementById('result').innerHTML = `Resultado: ${result}`;
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
+  } catch (error) {
+    document.getElementById('result').innerHTML = `Resultado: ${error.message}`;
+  }
+  
 }
+
 window.onload = () => {
   const button = document.getElementById('button');
   button.addEventListener('click', sum);
 }
+
