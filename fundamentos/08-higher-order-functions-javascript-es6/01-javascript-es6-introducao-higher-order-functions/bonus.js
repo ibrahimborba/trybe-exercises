a4c0eb197c6020dd5d0ffc160ec3eb65ac218ad3
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-03-10 16:07:49 -0300

    feat(requisito): bonus parte 1 requisito 1

---
+++ fundamentos/08-higher-order-functions-javascript-es6/01-javascript-es6-introducao-higher-order-functions/bonus.js
@@ -0,0 +1,27 @@
+// PARTE 1
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+// Requisito 1 - retorna dano do dragão
+const dragonDamage = (strength) => {
+  Math.floor(Math.random() * (strength - 15)) + 15;
+}
