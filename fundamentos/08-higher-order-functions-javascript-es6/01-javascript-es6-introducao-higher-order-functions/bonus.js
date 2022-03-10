04a3eda84679216ed7466a3a5e9094540e1fc00e
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-03-10 16:10:52 -0300

    feat(requisito): bonus parte 1 requisito 2

--- fundamentos/08-higher-order-functions-javascript-es6/01-javascript-es6-introducao-higher-order-functions/bonus.js
+++ fundamentos/08-higher-order-functions-javascript-es6/01-javascript-es6-introducao-higher-order-functions/bonus.js
@@ -23,5 +23,10 @@ const battleMembers = { mage, warrior, dragon };
 
 // Requisito 1 - retorna dano do dragão
 const dragonDamage = (strength) => {
-  Math.floor(Math.random() * (strength - 15)) + 15;
+  Math.floor(Math.random() * (strength - 14)) + 15;
+}
+
+// Requisito 2 - retorna dano do warrior
+const dragonDamage = (strength) => {
+  Math.floor(Math.random() * (strength * weaponDmg - strength + 1)) + strength;
 }
