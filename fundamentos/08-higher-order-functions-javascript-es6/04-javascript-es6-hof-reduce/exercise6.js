a902abb14a3a75ac6f30197614146ded0b1ec099
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-03-14 10:22:52 -0300

    feat(requisito): exercicios 8.4 requisito 6

---
+++ fundamentos/08-higher-order-functions-javascript-es6/04-javascript-es6-hof-reduce/exercise6.js
@@ -0,0 +1,12 @@
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage(students, grades) {
+  // escreva seu código aqui
+  return students.map((student, index) => ({
+    name: student,
+    average: grades[index].reduce((acc, elem) => acc += elem, 0) / grades[index].length,
+  }))
+
+}
+console.log(studentAverage(students, grades));
