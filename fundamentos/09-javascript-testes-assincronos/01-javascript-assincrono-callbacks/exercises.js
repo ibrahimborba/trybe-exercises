2aed74f555c3451e7e3087d0d65bb798ac166a7c
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-03-21 15:32:08 -0300

    feat(requisito): exercicios 9.1 requisito 3

---
+++ fundamentos/09-javascript-testes-assincronos/01-javascript-assincrono-callbacks/exercises.js
@@ -0,0 +1,13 @@
+// Exercício 3 - imprime Marte depois de 4 segundos
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  console.log("Returned planet: ", mars);
+};
+
+setTimeout(() => getPlanet(), 4000);
