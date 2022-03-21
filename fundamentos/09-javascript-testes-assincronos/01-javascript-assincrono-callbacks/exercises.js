507d7916b04deb5a1b56b6e61df8e515c06c62fe
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-03-21 15:36:03 -0300

    feat(requisito): exercicios 9.1 requisito 4

--- fundamentos/09-javascript-testes-assincronos/01-javascript-assincrono-callbacks/exercises.js
+++ fundamentos/09-javascript-testes-assincronos/01-javascript-assincrono-callbacks/exercises.js
@@ -10,4 +10,19 @@ const getPlanet = () => {
   console.log("Returned planet: ", mars);
 };
 
-setTimeout(() => getPlanet(), 4000);
+//setTimeout(() => getPlanet(), 4000);
+
+// Exercício 4 - 
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = (callback) => {
+  console.log(`Mars temperature is: ${callback()} degree Celsius`);
+}
+
+setTimeout(() => sendMarsTemperature(getMarsTemperature), messageDelay());// imprime "Mars temperature is: 20 degree Celsius", por exemplo
