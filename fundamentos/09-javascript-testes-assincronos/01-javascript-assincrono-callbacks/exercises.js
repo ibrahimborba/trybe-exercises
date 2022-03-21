d725540e7e97d6d23364d70124db55f195ca6b56
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-03-21 16:10:11 -0300

    feat(requisito): exercicios 9.1 requisito 5

--- fundamentos/09-javascript-testes-assincronos/01-javascript-assincrono-callbacks/exercises.js
+++ fundamentos/09-javascript-testes-assincronos/01-javascript-assincrono-callbacks/exercises.js
@@ -12,8 +12,8 @@ const getPlanet = () => {
 
 //setTimeout(() => getPlanet(), 4000);
 
-// Exercício 4 - 
-const messageDelay = () => Math.floor(Math.random() * 5000);
+// Exercício 4 - imprime a temperatura em Marte com um delay de até 5s
+/* const messageDelay = () => Math.floor(Math.random() * 5000);
 
 const getMarsTemperature = () => {
   const maxTemperature = 58;
@@ -25,4 +25,29 @@ const sendMarsTemperature = (callback) => {
   console.log(`Mars temperature is: ${callback()} degree Celsius`);
 }
 
-setTimeout(() => sendMarsTemperature(getMarsTemperature), messageDelay());// imprime "Mars temperature is: 20 degree Celsius", por exemplo
+setTimeout(() => sendMarsTemperature(getMarsTemperature), messageDelay()); */
+
+// Exercício 5 - 
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+// definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback) => {
+  const temperature = getMarsTemperature();
+  return callback(temperature);
+}
+
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
