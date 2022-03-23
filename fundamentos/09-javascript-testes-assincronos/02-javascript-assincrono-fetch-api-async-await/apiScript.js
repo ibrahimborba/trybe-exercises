553f5799aa5a2cbf79176f2768a3e3a30d4f8518
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-03-23 01:16:45 -0300

    feat(requisito): exercicios 9.2 requisito 1

--- fundamentos/09-javascript-testes-assincronos/02-javascript-assincrono-fetch-api-async-await/apiScript.js
+++ fundamentos/09-javascript-testes-assincronos/02-javascript-assincrono-fetch-api-async-await/apiScript.js
@@ -1,8 +1,17 @@
-// apiScript.js
+// apiScript.js     
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const joke = document.getElementById('jokeContainer');
+
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+  .then(response => response.json())
+  .then(data => joke.innerText = data.joke);
 };
 
 window.onload = () => fetchJoke();
