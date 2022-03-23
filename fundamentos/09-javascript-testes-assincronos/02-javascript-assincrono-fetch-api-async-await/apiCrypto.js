384005176733b8259d2377a58ebc075e73540293
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-03-23 03:07:35 -0300

    feat(requisito): exercicios 9.2 bonus

--- fundamentos/09-javascript-testes-assincronos/02-javascript-assincrono-fetch-api-async-await/apiCrypto.js
+++ fundamentos/09-javascript-testes-assincronos/02-javascript-assincrono-fetch-api-async-await/apiCrypto.js
@@ -1,19 +1,24 @@
-const appendCrypto = ({name, symbol, priceUsd}) => {
+const appendCrypto = ({name, symbol, priceUsd}, conversor) => {
   const cryptoList = document.getElementById('crypto-list');
-
+  const convertedPrice = parseFloat(priceUsd) * conversor;
   const listItem = document.createElement('li');
-  listItem.innerText = `${name} (${symbol}): ${parseFloat(priceUsd).toFixed(2)}`;
+  listItem.innerText = `${name} (${symbol}): ${convertedPrice.toFixed(2)}`;
   cryptoList.appendChild(listItem);
 }
 
-const fetchCrypto = () => {
+const fetchCurrencies = () => {
+  return fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json')
+  .then(response => response.json())
+  .then(data => data.usd.brl);
+}
+
+const addCrypto = async () => {
+  const conversor = await fetchCurrencies();
   fetch('https://api.coincap.io/v2/assets')
     .then(response => response.json())
     .then(data => data.data.filter((crypto) => crypto.rank <= 10)
-      .reduce((acc, crypto) => acc = appendCrypto(crypto), 0));
-  
-  return promise;
+      .reduce((acc, crypto) => acc = appendCrypto(crypto, conversor), 0));
 };
 
-fetchCrypto();
+addCrypto();
 
