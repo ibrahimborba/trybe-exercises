800be485f007191928c6799645b42357a2eb43e2
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-04-04 12:10:21 -0300

    initial commit

---
+++ front-end/01-introducao-react/01-hello-world-react/exercises/src/Component.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Component extends React.Component {
+  render () {
+    return <div><h1>Hello World!</h1><p>Exercício React</p></div> 
+  }
+};
+
+export default Component;
