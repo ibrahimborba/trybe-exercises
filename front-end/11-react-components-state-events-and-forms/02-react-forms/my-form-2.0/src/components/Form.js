2d95e8454ce33ba471fcd7e99ac5838013bc4fee
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-04-08 02:38:25 -0300

    feat: add NameInput component

--- front-end/11-react-components-state-events-and-forms/02-react-forms/my-form-2.0/src/components/Form.js
+++ front-end/11-react-components-state-events-and-forms/02-react-forms/my-form-2.0/src/components/Form.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from './Form.module.css';
+import NameInput from './NameInput';
 
 class Form extends Component {
   constructor() {
@@ -18,63 +19,68 @@ class Form extends Component {
       description: '',
     };
 
-    handleChange({ target }) {
-      const { name } = target;
-      const value = target.type === 'checkbox' ? target.checked : target.value;
-    
-      this.setState({
-        [name]: value,
-      });
-    }
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+  
+    this.setState({
+      [name]: value,
+    });
   }
 
   render () {
     return (
       <form>
         <fieldset className={styles.fieldset}>
-          <label>
-            Nome
-            <input
-              name='nameInput' type='text' maxLength='40'
-              value={this.state.nameInput}
-              onChange={this.handleChange}
-              style={{textTransform: 'uppercase'}}
-              required />
-          </label>
+          <NameInput value={this.state.nameInput} handleChange={this.handleChange}/>
           <label>
             Email
             <input
               name='email' type='text' maxLength='50'
+              value={this.state.email}
+              onChange={this.handleChange}
               required />
           </label>
           <label>
             CPF
             <input
               name='cpf' type='text' maxLength='11'
+              value={this.state.cpf}
+              onChange={this.handleChange}
               required />
           </label>
           <label>
             Endereço
             <input
               name='address' type='text' maxLength='200'
+              value={this.state.address}
+              onChange={this.handleChange}
               required />
           </label>
           <label>
             Cidade
             <input
               name='city' type='text' maxLength='28'
+              value={this.state.city}
+              onChange={this.handleChange}
               required />
           </label>
           <label>
             Estado
-            <select name="stateInput" id="state" required>
+            <select
+              name="stateInput" id="state"
+              value={this.state.stateInput}
+              onChange={this.handleChange}
+              required>
               <option value="uf">UF</option>
             </select>
           </label>
           <label>
             Casa
             <input
-              name='liveIn' type='radio' value='Casa'
+              name='house' type='radio' value='Casa'
               required />
           </label>
           <label>
@@ -88,15 +94,27 @@ class Form extends Component {
         <fieldset className={styles.fieldset}>
           <label>
             Resumo do currículo
-            <textarea name='curriculum' maxLength='1000' required />
+            <textarea
+              name='curriculum' maxLength='1000'
+              value={this.state.curriculum}
+              onChange={this.handleChange}
+              required />
           </label>
           <label>
             Cargo
-            <textarea name='jobTitle' maxLength='40' required />
+            <textarea
+            name='jobTitle' maxLength='40'
+            value={this.state.jobTitle}
+            onChange={this.handleChange}
+            required />
           </label>
           <label>
             Descrição do cargo
-            <textarea name='description' maxLength='500' required />
+            <textarea
+            name='description' maxLength='500'
+            value={this.state.description}
+            onChange={this.handleChange}
+            required />
           </label>
         </fieldset>
       </form>
