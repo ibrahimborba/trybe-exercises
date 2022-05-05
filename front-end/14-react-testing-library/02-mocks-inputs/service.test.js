a4f5796e1ba906cf35393169de33fc2011451264
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-05-05 03:03:26 -0300

    feat: change functionality again

--- front-end/14-react-testing-library/02-mocks-inputs/service.test.js
+++ front-end/14-react-testing-library/02-mocks-inputs/service.test.js
@@ -1,9 +1,9 @@
 it("tests randomNumber", () => {
   // testando se a função foi chamada e qual seu retorno
-  randomNumber = jest.fn().mockImplementation((a, b) => a / b);
+  randomNumber = jest.fn().mockImplementation((a) => a * 2);
 
-  randomNumber(4, 2);
+  randomNumber(4);
   expect(randomNumber).toHaveBeenCalled();
   expect(randomNumber).toHaveBeenCalledTimes(1);
-  expect(randomNumber(4, 2)).toBe(2);
+  expect(randomNumber(4)).toBe(8);
 });
