d3d7b1a614ec7d50440ee91c1c25ab80fe7ad7ed
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-05-05 03:06:51 -0300

    feat: add mockReset

--- front-end/14-react-testing-library/02-mocks-inputs/service.test.js
+++ front-end/14-react-testing-library/02-mocks-inputs/service.test.js
@@ -5,5 +5,15 @@ it("tests randomNumber", () => {
   randomNumber(4);
   expect(randomNumber).toHaveBeenCalled();
   expect(randomNumber).toHaveBeenCalledTimes(1);
+  expect(randomNumber).toHaveBeenLastCalledWith(4);
   expect(randomNumber(4)).toBe(8);
+
+  randomNumber.mockReset();
+  randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+  randomNumber(4, 2, 1);
+  expect(randomNumber).toHaveBeenCalled();
+  expect(randomNumber).toHaveBeenCalledTimes(1);
+  expect(randomNumber).toHaveBeenLastCalledWith(4, 2, 1);
+  expect(randomNumber(4, 2, 1)).toBe(8);
+
 });
