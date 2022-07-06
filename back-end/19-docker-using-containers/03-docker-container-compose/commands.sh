78fc8644d0c1c44bdf2f2e5db172883c62d296fa
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-07-06 16:10:29 -0300

    feat: add log, down up one service, up building commands

--- back-end/19-docker-using-containers/03-docker-container-compose/commands.sh
+++ back-end/19-docker-using-containers/03-docker-container-compose/commands.sh
@@ -1,2 +1,18 @@
-# Up docker compose
+# Up (build, start) services
 docker-compose up -d
+
+# Up forcing build (updates if a service has been altered)
+docker-compose up -d --build
+
+# Visualize service status
+docker-compose ps
+
+# Down (stop, remove) services
+docker-compose down
+
+# Up one service
+docker-compose up <service>
+
+#See service logs
+docker-compose logs <service>
+docker-compose logs --tail <number> <service>
