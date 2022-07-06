f65832287dcfdd358ff6fe915b3b16953ca3ccce
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-07-06 15:09:07 -0300

    feat: add image prune command

--- back-end/19-docker-using-containers/02-manipulating-docker-images/commands.sh
+++ back-end/19-docker-using-containers/02-manipulating-docker-images/commands.sh
@@ -4,3 +4,7 @@ docker build -t <image name> .
 #Create new container with web server
 docker build -t meu-servidor-web .
 docker run --rm -d -p 1234:80 --name meu-container meu-servidor-web
+
+
+#Removing images
+docker system prune -af
