90ee9bebdb0bfe4581070837c8221945c35e2645
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-07-06 03:30:55 -0300

    feat: add multiple stages

--- back-end/19-docker-using-containers/02-manipulating-docker-images/hugo/commands.sh
+++ back-end/19-docker-using-containers/02-manipulating-docker-images/hugo/commands.sh
@@ -3,3 +3,6 @@ docker build -t site-hugo .
 
 #Create container
 docker run -p 1234:80 -d --name meu-container site-hugo
+
+#Create image with multiple stages
+docker build -t multi-stage-site-hugo .
