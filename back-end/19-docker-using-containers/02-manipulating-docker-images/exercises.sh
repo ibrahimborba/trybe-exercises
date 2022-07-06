95d9c9b47366d52d14aa4ef9cb299df3dc548fb2
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-07-06 03:05:32 -0300

    feat: add command to create web server

--- back-end/19-docker-using-containers/02-manipulating-docker-images/exercises.sh
+++ back-end/19-docker-using-containers/02-manipulating-docker-images/exercises.sh
@@ -1,2 +1,5 @@
 #Create an image
-docker build -t primeira-imagem .
+docker build -t <image name> .
+
+#Create new container with web server
+docker run --rm -d -p 1234:80 --name meu-container meu-servidor-web
