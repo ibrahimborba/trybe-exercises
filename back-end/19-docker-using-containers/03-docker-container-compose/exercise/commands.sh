ac13b7d148dc8437c2d9cbc7a9d77cd1c329268c
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-07-07 14:48:39 -0300

    feat: create new folder for practical exercises and solve exercise 1

---
+++ back-end/19-docker-using-containers/03-docker-container-compose/exercise/commands.sh
@@ -0,0 +1,6 @@
+#Exercise 1 -Create a volume from httpd Apache image
+docker run -d --name site-trybe -p 4545:80 -v /home/ibrahim/Documents/Trybe/trybe-exercises/back-end/19-docker-using-containers/03-docker-container-compose/exercise:/usr/local/apache2/htdocs/ httpd:2.4
+#Path
+http://localhost:4545/missao_trybe.html
+#Inspect and check Source in Mount
+docker inspect site-trybe
