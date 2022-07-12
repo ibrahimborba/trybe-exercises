d287ee754d86c5649a7b562e28bd122e2ae2bae8
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-07-12 19:29:10 -0300

    initial commit: create branch

--- back-end/19-docker-using-containers/03-docker-container-compose/exercise/commands.sh
+++ back-end/19-docker-using-containers/03-docker-container-compose/exercise/commands.sh
@@ -1,5 +1,5 @@
 #Exercise 1 -Create a volume from httpd Apache image
-docker run -d --name site-trybe -p 4545:80 -v /home/ibrahim/Documents/Trybe/trybe-exercises/back-end/19-docker-using-containers/03-docker-container-compose/exercise:/usr/local/apache2/htdocs/ httpd:2.4
+docker run -d --name site-trybe -p 4545:80 -v $(pwd):/usr/local/apache2/htdocs/ httpd:2.4
 #Path
 http://localhost:4545/missao_trybe.html
 #Inspect and check Source in Mount
