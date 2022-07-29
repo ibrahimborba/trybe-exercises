bc69e59922d2029a8f51c1ed9b70046c10db0655
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-07-29 16:07:27 -0300

    feat: add route with query string

--- back-end/22-introduction-web-development-nodejs/04-express-nodejs-http/hello-express/index.js
+++ back-end/22-introduction-web-development-nodejs/04-express-nodejs-http/hello-express/index.js
@@ -17,6 +17,30 @@ const drinks = [
 	{ id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
+app.get('/recipes/search', function (req, res) {
+	const { name, maxPrice } = req.query;
+	const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price < Number(maxPrice));
+  res.status(200).json(filteredRecipes);
+});
+
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === Number(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+
+  res.status(200).json(recipe);
+});
+
+app.get('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const drink = drinks.find((d) => d.id === Number(id));
+
+  if (!drink) return res.status(404).json({ message: 'Drink not found!'});
+
+  res.status(200).json(drink);
+});
+
 app.get('/recipes', function (req, res) {
   res.json(recipes);
 });
