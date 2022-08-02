9473132a1878499f99c38c9a28321c6a85051f2a
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-08-02 01:38:09 -0300

    feat: add post request for name

--- back-end/22-introduction-web-development-nodejs/04-express-nodejs-http/practice/app.js
+++ back-end/22-introduction-web-development-nodejs/04-express-nodejs-http/practice/app.js
@@ -7,6 +7,11 @@ app.get('/ping', (_req, res) => {
   res.status(200).json({ message: 'pong' });
 });
 
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ message: `Hello, ${name}!` });
+});
+
 module.exports = {
   app,
 };
