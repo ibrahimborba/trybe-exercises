06ed231837eb1fbc1b189fee44b692c236c091c7
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-08-02 01:50:06 -0300

    feat: add put request with user  name and age

--- back-end/22-introduction-web-development-nodejs/04-express-nodejs-http/practice/app.js
+++ back-end/22-introduction-web-development-nodejs/04-express-nodejs-http/practice/app.js
@@ -20,6 +20,11 @@ app.post('/greetings', (req, res) => {
   res.status(200).json({ message: `Hello, ${name}!` });
 });
 
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
+
 module.exports = {
   app,
 };
