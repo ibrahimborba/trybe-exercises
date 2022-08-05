d464a70ac8384ade12d832d644540c19a1d6e5a0
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-08-05 02:44:40 -0300

    feat: add /books endpoint

--- back-end/23-nodejs-service-layer-and-rest-restful-architecture/model-example/index.js
+++ back-end/23-nodejs-service-layer-and-rest-restful-architecture/model-example/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const books = require('./models/books');
 const app = express();
 app.use(express.json());
 
 const PORT = 3001;
+
+app.get('/books', async (_req, res) => {
+  try {
+    const books = await books.getAll();
+    res.status(200).json(books);
+    
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
+});
 
 app.listen(PORT, () => console.log(`Listening port ${PORT}`));
