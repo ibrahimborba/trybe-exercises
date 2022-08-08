7fa984d8eacfbfe1a695b52e9293c14aa8183781
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-08-08 11:11:50 -0300

    feat: add env varaibles

--- back-end/23-nodejs-service-layer-and-rest-restful-architecture/02-software-architecture-controller-service/cep-lookup/index.js
+++ back-end/23-nodejs-service-layer-and-rest-restful-architecture/02-software-architecture-controller-service/cep-lookup/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+require('dotenv').config();
 const { pong } = require('./controllers/pong.controller');
 
 const app = express();
 
 const { log } = console;
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/ping', pong);
 
