4b6defc1ef2f031ab509e3d57a1c842969878c67
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-08-23 00:40:06 -0300

    feat: change relation to hasMany

--- back-end/24-nodejs-orm-authentication/02-orm-associations/index.js
+++ back-end/24-nodejs-orm-authentication/02-orm-associations/index.js
@@ -17,7 +17,7 @@ app.get('/employees', async (_req, res) => {
   };
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
 
 module.exports = app;
